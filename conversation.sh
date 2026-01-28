606721f54d5504435edb58c4522d2d2fe94f065e
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:29:19 +0530

    latest commit

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Ela vunnav Ramesh mawa?"
+echo "Ramesh: Bavunna mawa nuvvu ela vunnav?"
+echo "Suresh: Bavunna ra inkenti sangathulu"
+echo "Ramesh: All good ra that's it"
