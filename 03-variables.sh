70f4bb1324739c9720c1ce7e9337584eb4de2e86
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:33:15 +0530

    latest commit

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Suresh
+Person2=Ramesh
+echo "$Person1: Ela vunnav ${Person2} mawa?"
+echo "Ramesh: Bavunna mawa nuvvu ela vunnav?"
+echo "$Person1: Bavunna ra inkenti sangathulu"
+echo "$Person2: All good ra that's it"
+
