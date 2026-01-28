9ed0ae02c7f96881bc9406acbe7a0dd6bb3b1660
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:53:55 +0530

    latest commit

--- login.sh
+++ login.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+login {
 echo "Please Enter your username::"
 read USER_NAME
 echo "Your Username is $USER_NAME"
 
 echo "Enter the password::"
 read -s PASSWORD
-echo "kikiki chepestam mari"
+echo "Login successful :)"
+
+}
+echo "Press 1 to Login 2 to exit"
+read OPTION
+if [$OPTION -eq 1 ]; then
+   login()
+elif [$OPTION -eq 2 ]; then
+   exit 1;
+
