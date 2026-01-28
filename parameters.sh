114fa2b215a33d6e5adad711de33171f6a0e7044
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:42:17 +0530

    latest commit

--- parameters.sh
+++ parameters.sh
@@ -2,8 +2,10 @@
 
 Person1=$1
 Person2=$2
+Person3=$3
 echo "$Person1: Ela vunnav ${Person2} mawa?"
 echo "${Person2}: Bavunna mawa nuvvu ela vunnav?"
 echo "$Person1: Bavunna ra inkenti sangathulu"
 echo "$Person2: All good ra that's it"
 
+echo "$Person3: Get out $Person1"
