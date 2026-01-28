ff557fd1329b43102480f37be09dc53f13cac63e
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:39:14 +0530

    latest commit

---
+++ parameters.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+echo "$Person1: Ela vunnav ${Person2} mawa?"
+echo "${Person2}: Bavunna mawa nuvvu ela vunnav?"
+echo "$Person1: Bavunna ra inkenti sangathulu"
+echo "$Person2: All good ra that's it"
+
