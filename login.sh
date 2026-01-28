96ed90ac382cac1d2377772e916f585761cb6d92
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-28 18:47:49 +0530

    latest commit

--- login.sh
+++ login.sh
@@ -3,3 +3,8 @@
 echo "Please Enter your username::"
 read USER_NAME
 echo "Your Username is $USER_NAME"
+
+echo "Enter the password::"
+read -s PASSWORD
+echo "Your Password is $PASSWORD :)"
+echo "kikiki chepestam mari"
