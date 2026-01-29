87495e81213d820c9b39da372763fc5e595b1d76
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-29 18:27:39 +0530

    latest commit

--- colors.sh
+++ colors.sh
@@ -8,8 +8,8 @@ LOG_FILE="/var/log/shell-sctipt-logs/$0.log"
 
 R="\e[31m"
 G="\e[32m"
-B="\e[33m"
-Y="\e[34m"
+Y="\e[33m"
+B="\e[34m"
 N="\e[0m"
 
 mkdir -p $LOG_FOLDER
@@ -18,16 +18,16 @@ if [ $USER_ID -ne 0 ]; then
    echo -e "$R You are not a Root user $N" | tee -a $LOG_FILE
    exit 1
 else
-   echo -e "$g You are a ROOT USER $N"
+   echo -e "$G You are a ROOT USER $N"
 
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-       echo -e"$2.... is $R Failed     :( $N" | tee -a $LOG_FILE
+       echo -e"$2....$R Failed     :( $N" | tee -a $LOG_FILE
        exit 1
     else
-       echo -e "$2.... is $G Successful :) $N" | tee -a $LOG_FILE
+       echo -e "$2....$G Successful :) $N" | tee -a $LOG_FILE
     fi
 
 }
