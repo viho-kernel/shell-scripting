5a9ae014da49f1b570644762bb8416ee29f62698
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-29 17:28:35 +0530

    latest commit

--- logs.sh
+++ logs.sh
@@ -5,7 +5,7 @@ LOG_FILE="/var/log/shell-sctipt-logs/$0.log"
 mkdir -p $LOG_FOLDER
 
 if [ $USER_ID -ne 0 ]; then
-   echo "You are not a Root user"
+   echo "You are not a Root user" | tee -a $LOG_FILE
    exit 1
 else
    echo "You are a ROOT USER"
@@ -14,10 +14,10 @@ fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-       echo "$2.... Failed :("
+       echo "$2.... Failed :(" | tee -a $LOG_FILE
        exit 1
     else
-       echo "$2.... Success :) "
+       echo "$2.... Success :) " | tee -a $LOG_FILE
     fi
 
 }
