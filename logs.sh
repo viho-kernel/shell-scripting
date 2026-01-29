aecd8ec7482a4b8dfdd7dc75bd857f666bfc41f5
viho-kernel <vihari.reddy1802@gmail.com>
2026-01-29 17:21:17 +0530

    latest commit

--- logs.sh
+++ logs.sh
@@ -2,7 +2,7 @@ USER_ID=$(id -u)
 LOG_FOLDER="/var/log/shell-sctipt-logs"
 LOG_FILE="/var/log/shell-sctipt-logs/$0.log"
 
-mkdir -p LOG_FOLDER
+mkdir -p $LOG_FOLDER
 
 if [ $USER_ID -ne 0 ]; then
    echo "You are not a Root user"
@@ -22,14 +22,14 @@ VALIDATE() {
 
 }
 
-dnf install nginx -y &>> LOG_FILE
+dnf install nginx -y &>> $LOG_FILE
 
 VALIDATE $? "Installing Nginx"
 
-dnf install mysql -y &>> LOG_FILE
+dnf install mysql -y &>> $LOG_FILE
 
 VALIDATE $? "Installing mysql"
 
-dnf install httpds -y &>> LOG_FILE
+dnf install httpds -y &>> $LOG_FILE
 
 VALIDATE $? "Installing httpd"
