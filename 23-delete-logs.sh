99b800ec17452c0f49d200d0b3c63827f751574a
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-04 09:54:57 +0530

    latest commit

---
+++ 23-delete-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOG_FOLDER=/root/app-logs/
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+
+#check if directory present or not
+
+if [ ! -d $LOG_FOLDER ];then
+   echo -e "$LOG_FOLDER does not exist"
+   exit 1
+fi
+
+FILES_TO_DELETE=$(find $LOG_FOLDER -name "*.log" -mtime +14)
+echo "$FILES_TO_DELETE"
