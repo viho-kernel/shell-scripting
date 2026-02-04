0238f057661fadb190ab8d17e493c5f6336e895b
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-04 09:57:33 +0530

    latest commit

--- 23-delete-logs.sh
+++ 23-delete-logs.sh
@@ -7,10 +7,10 @@ LOG_FILE="$LOG_FOLDER/$0.log"
 #check if directory present or not
 
 if [ -d $LOG_FOLDER ];then
+   echo -e "$LOG_FOLDER does exist"
+else 
    echo -e "$LOG_FOLDER does not exist"
    exit 1
-else 
-   echo -e "$LOG_FOLDER does exist"
 fi
 
 FILES_TO_DELETE=$(find $LOG_FOLDER -name "*.log" -mtime +14)
