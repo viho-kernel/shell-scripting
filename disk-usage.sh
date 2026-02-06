9ded0d6d50950a344dca2ff9822997495b04b517
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-06 09:47:55 +0530

    latest commit

--- disk-usage.sh
+++ disk-usage.sh
@@ -9,18 +9,18 @@ Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 MESSAGE=""
+
 log(){
-    echo -e "$(date "+%Y-%m-%d %H:%M%S") | $1" | tee -a $LOG_FILE
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOG_FILE
 }
 
-DISK_USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}' | cur -d "%" -f1 )
+DISK_USAGE=$(df -hT | grep -v Filesystem)
 USAGE_THRESHOLD=10
 
 while IFS= read -r line
 do
-  
-   USAGE=$line
-   PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}')
+   USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+   PARTITION=$(echo $line | awk '{print $7}')
 
    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE+="High Disk Usage on $PARTITION: $USAGE"
