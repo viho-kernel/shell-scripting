5287dc8f682636c816a6ded96e8c90b52132ac1c
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-06 11:10:51 +0530

    latest commit

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,7 +6,9 @@ Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
 MESSAGE=""
-
+Instance_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+SUBJECT="High Disk Utilization Alert 🚨 on ${Instance_ID}"
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 log(){
     echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOG_FILE
 }
@@ -28,3 +30,5 @@ do
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
+
+sh mail.sh "vihari.reddy1802@gmail.com" "${SUBJECT}" "${MESSAGE}" "DISK_UTILIZATION" "${IP_ADDRESS}" "DevOps Team" "${Instance_ID}"
---
+++ mail.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TO_ADDRESS=$1
+SUBJECT=$2
+MESSAGE_BODY=$3
+ALERT_TYPE=$4
+SERVER_IP=$5
+TO_TEAM=$6
+Instance_ID=$7
+
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$TO_Team/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/server/$Instance_ID/g" -e "s/SERVER_IP/$SERVER_IP/g" -e "s/MESSAGE/$MESSAGE_BODY/g" template.html)
+
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_MESSAGE"
+} | msmtp "$TO_ADDRESS"
