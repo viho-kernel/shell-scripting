0058bf0f736ed67262c8c5db370df28ecec7498f
viho-kernel <vihari.reddy1802@gmail.com>
2026-02-06 09:41:59 +0530

    latest commit

--- disk-usage.sh
+++ disk-usage.sh
@@ -25,7 +25,7 @@ do
    USAGE=$line
    PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}')
 
-   if [ "$USAGE" -gt "$PARTITION" ]; then
+   if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
         MESSAGE+="High Disk Usage on $PARTITION: $USAGE"
    fi
 
